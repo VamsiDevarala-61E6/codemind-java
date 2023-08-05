11b5a0817c89d88c76e7d2d6b74e4f632185c115
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2023-08-05 11:46:23 +0530

    import java.util.Scanner; public class main{     public static void main(String[] args){         Scanner v=new Scanner(System.in);         double a,b;         a=v.nextDouble();         b=v.nextDouble();         System.out.printf("%.2f",a*b);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args){
+        Scanner v=new Scanner(System.in);
+        double a,b;
+        a=v.nextDouble();
+        b=v.nextDouble();
+        System.out.printf("%.2f",a*b);
+    }
+}
