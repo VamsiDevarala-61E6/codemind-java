f4fa548b9f71d41c4d0c0c9486d66057babdfbf0
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2023-08-05 12:08:21 +0530

    import java.util.Scanner; public class Vamsi{     public static void main(String[] args){         Scanner n=new Scanner(System.in);         String s;         s=n.nextLine();         System.out.print(s);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Vamsi{
+    public static void main(String[] args){
+        Scanner n=new Scanner(System.in);
+        String s;
+        s=n.nextLine();
+        System.out.print(s);
+    }
+}
