b5b402be3620b169d33bcb3c7f1648bc8708d6e4
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2023-08-05 11:51:57 +0530

    import java.util.Scanner; public class Vamsi{     public static void main(String[] arg){         Scanner v=new Scanner(System.in);         int a,b;         a=v.nextInt();         b=v.nextInt();         System.out.println(a*b);     } }

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Vamsi{
+    public static void main(String[] arg){
+        Scanner v=new Scanner(System.in);
+        int a,b;
+        a=v.nextInt();
+        b=v.nextInt();
+        System.out.println(a*b);
+    }
+}
