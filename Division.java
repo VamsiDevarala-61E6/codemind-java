1ff62bb1c3dd9bec87bb46d5302af243838359fa
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2023-08-05 11:59:18 +0530

    import java.util.Scanner; public class Vamsi{     public static void main(String[] args){         Scanner n=new Scanner(System.in);         int a,b;         a=n.nextInt();         b=n.nextInt();         System.out.print(a/b);     } }

---
+++ Division.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Vamsi{
+    public static void main(String[] args){
+        Scanner n=new Scanner(System.in);
+        int a,b;
+        a=n.nextInt();
+        b=n.nextInt();
+        System.out.print(a/b);
+    }
+}
