1ac5c401e305b195bbc97457b0cebe605122c97c
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2023-08-09 09:57:54 +0530

    import java.util.Scanner; public class Main{     public static void main(String[] args){         Scanner v=new Scanner(System.in);         char ch;         ch=v.next().charAt(0);         int k;         k=(int)ch;         System.out.print(k);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner v=new Scanner(System.in);
+        char ch;
+        ch=v.next().charAt(0);
+        int k;
+        k=(int)ch;
+        System.out.print(k);
+    }
+}
