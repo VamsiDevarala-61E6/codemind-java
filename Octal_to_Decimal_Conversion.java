f3f47cc763a787de21a96df91a089084f3620b6b
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2023-08-13 10:26:10 +0530

    import java.util.Scanner; public class Vamsi{     public static void main(String[] args){         Scanner v=new Scanner(System.in);         String s;         s=v.next();         int n=Integer.parseInt(s,8);         System.out.print(n);     } }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Vamsi{
+    public static void main(String[] args){
+        Scanner v=new Scanner(System.in);
+        String s;
+        s=v.next();
+        int n=Integer.parseInt(s,8);
+        System.out.print(n);
+    }
+}
