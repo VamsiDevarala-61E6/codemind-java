6c04f147a75d89c27c511a83dee936a509978e94
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2023-08-26 17:13:49 +0530

    import java.util.Scanner; public class Main{     public static void main(String[] args){         Scanner v=new Scanner(System.in);         int n,k;         n=v.nextInt();         int[] x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=v.nextInt();         }         k=v.nextInt();         int c=0;         for(int i:x)         {             if(prime(i))             {                 if(i>=k)                 {                     c++;                 }             }         }         System.out.println(c);
        }
        public static boolean prime(int n)
        {
            if(n==1)
              return false;
            for(int i=2;i<=Math.sqrt(n);i++)
            {
                if(n%i==0)
                  return false;
            }
            return true;
        }
    }

---
+++ Primes_greater_than_k.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner v=new Scanner(System.in);
+        int n,k;
+        n=v.nextInt();
+        int[] x=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=v.nextInt();
+        }
+        k=v.nextInt();
+        int c=0;
+        for(int i:x)
+        {
+            if(prime(i))
+            {
+                if(i>=k)
+                {
+                    c++;
+                }
+            }
+        }
+        System.out.println(c);
+        
+    }
+    public static boolean prime(int n)
+    {
+        if(n==1)
+          return false;
+        for(int i=2;i<=Math.sqrt(n);i++)
+        {
+            if(n%i==0)
+              return false;
+        }
+        return true;
+    }
+}
