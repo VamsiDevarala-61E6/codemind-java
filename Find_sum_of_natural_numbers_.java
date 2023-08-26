bb6c11d44dbd9822befff4acb654cd48ae1af13d
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2023-08-26 06:46:24 +0530

    import java.util.Scanner; public class Main{     public static void main(String[] args){         Scanner v=new Scanner(System.in);         int n;         n=v.nextInt();         System.out.println(n*(n+1)/2);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner v=new Scanner(System.in);
+        int n;
+        n=v.nextInt();
+        System.out.println(n*(n+1)/2);
+    }
+}
