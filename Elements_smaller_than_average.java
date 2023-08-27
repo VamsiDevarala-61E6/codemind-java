389dd24b4d2a8b7395807d1cd43cb2d87ab7c5c6
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2023-08-27 19:22:42 +0530

    import java.util.Scanner; public class Main{     public static void main(String[] args){         Scanner v=new Scanner(System.in);         int n=v.nextInt();         int[] x=new int[n];         int s=0,c=0;         for(int i=0;i<n;i++)         {             x[i]=v.nextInt();             s+=x[i];         }         int k=s/n;         for(int i=0;i<n;i++)         {             if(x[i]<=k)             {                 c++;             }         }         System.out.println(c);     } }

---
+++ Elements_smaller_than_average.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner v=new Scanner(System.in);
+        int n=v.nextInt();
+        int[] x=new int[n];
+        int s=0,c=0;
+        for(int i=0;i<n;i++)
+        {
+            x[i]=v.nextInt();
+            s+=x[i];
+        }
+        int k=s/n;
+        for(int i=0;i<n;i++)
+        {
+            if(x[i]<=k)
+            {
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
