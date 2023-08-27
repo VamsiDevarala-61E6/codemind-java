03ca2c93ad7388d332be228543e34a4783a5a0d6
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2023-08-27 19:29:56 +0530

    import java.util.Scanner; public class Main{     public static void main(String[] args){         Scanner v=new Scanner(System.in);         int n=v.nextInt();         int[] x=new int[n];         int s=0,c=0;         for(int i=0;i<n;i++)         {             x[i]=v.nextInt();             if(x[i]%2==0)             {                 c=i;             }         }         System.out.println(c);     } }

---
+++ Last_even_element_index.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner v=new Scanner(System.in);
+        int n=v.nextInt();
+        int[] x=new int[n];
+        int s=0,c=0;
+        for(int i=0;i<n;i++)
+        {
+            x[i]=v.nextInt();
+            if(x[i]%2==0)
+            {
+                c=i;
+            }
+        }
+        System.out.println(c);
+    }
+}
