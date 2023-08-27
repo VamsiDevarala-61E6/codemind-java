edabfe96da9e299ffb4ba04477644b4f230dff00
VAMSI DEVARALA <119839709+VamsiDevarala-61E6@users.noreply.github.com>
2023-08-27 19:02:38 +0530

    import java.util.Scanner; public class Main{     public static void main(String[] args){         Scanner v=new Scanner(System.in);         int n=v.nextInt();         int[] x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=v.nextInt();         }         int c,s=0;         for(int i=0;i<n;i++)         {             c=0;             for(int j=i+1;j<n;j++)             {                 if(x[i]==x[j])                 {                     c++;                 }             }             if(c==0)             {                 if(x[i]%2!=0)                 {                     s+=x[i];                 }             }         }         if(s>0)          System.out.println(s);         else          System.out.println("-1");
        }
    }

---
+++ unique_odd_elements_sum.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner v=new Scanner(System.in);
+        int n=v.nextInt();
+        int[] x=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=v.nextInt();
+        }
+        int c,s=0;
+        for(int i=0;i<n;i++)
+        {
+            c=0;
+            for(int j=i+1;j<n;j++)
+            {
+                if(x[i]==x[j])
+                {
+                    c++;
+                }
+            }
+            if(c==0)
+            {
+                if(x[i]%2!=0)
+                {
+                    s+=x[i];
+                }
+            }
+        }
+        if(s>0)
+         System.out.println(s);
+        else
+         System.out.println("-1");
+
+    }
+}
